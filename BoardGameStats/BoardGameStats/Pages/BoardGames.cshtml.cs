21463a83195e923aba052f50cb8e4df01ce588ee
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-11-15 18:15:29 -0700

    Initial creation of pages

---
+++ BoardGameStats/BoardGameStats/Pages/BoardGames.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoardGameStats.Pages
+{
+    public class BoardGamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BoardGameStats/BoardGameStats/Pages/Players.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoardGameStats.Pages
+{
+    public class PlayersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
